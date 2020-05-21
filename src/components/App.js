87dbeb802da2e83c46bde214345180b68e4d8d4c
Mitch Lum <mklum88@gmail.com>
2020-05-21 13:34:34 -0700

    added inline styling margin top

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import SongDetail from './SongDetail';
 
 const App = () => {
   return (
-    <div className='ui container grid'>
+    <div className='ui container grid' style={{ marginTop: '25px' }}>
       <div className='ui row'>
         <div className='column eight wide'>
           <SongList />
